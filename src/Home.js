d4391eded814a189afed04c694254e6395b2ee23
SaikumarK1-hexaware <1000071583@hexaware.com>
2023-10-16 22:00:51 +0530

    changes of first app

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import { Button } from "@mui/material";
+import images from './images.json';
+
+export default function Home(){
+    return (
+        <>
+            <Button>MyHome</Button><br/>
+            <Button>Helllooo</Button>
+                         
+        </>     
+    )
+
+
+}
